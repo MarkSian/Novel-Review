4a50fe0e5f13b8fbba9802b4d78b25ab81922ea1
MarkSian <merksianipar@gmail.com>
2025-05-01 19:14:30 -0600

    server.mjs framing completed

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # South Park API
 A database for the show South Park
+
+1) Install dependencies **
+2) Create Data Folder to hold sample data **
+3) Import and frame server.mjs **
+4) Fill in the framing for server.mjs **
+5) Create routes folder for routes
+6) Create models folder
+7) Create DB folder and conn.mjs file to connect to MongoDB
--- server.mjs
+++ server.mjs
@@ -1,23 +1,28 @@
 import express from 'express';
 import dotenv from 'dotenv';
 
-// Setups
-dotenv.config();
-const app = express();
-const PORT = process.env.PORT || 3001;
+// Setup: Here we import the necessary modules. express() is a web framework for Node.js, and dotenv is used to load environment variables from the .evn file. Such as PORT and our DB_URI.
+dotenv.config(); // dotenv.config() allows us to use the variables from the .env file
+const app = express(); // express() creates an instance of the express application, allowing us to define routes, middleware, and other configurations
+const PORT = process.env.PORT || 3001; // PORT is set to the value of the PORT within the .env file.
+// Middleware: Middleware functions serve as a bridge between the request and response cycle. They can modify the request, perform actions, or end the request-response cycle.
+app.use(express.json()); // app.use(express.json()) allows us to parse incoming request bodies in a JSON format, making it easier to work with JSON data in our application.
+app.use(express.urlencoded({ extended: true })); // app.use(express.urlencoded({ extended: true })) allows us to parse incoming request bodies in the URL-encoded format, which is commonly used for form submissions. 
+// extended:true allows for the parsing of more complex objects, while false can only parse simple objects. Complex such as nested objects.
 
-// Middlewares
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+
+// Routes
 
 
-// Error handling
-app.use((err, req, res, next) => {
+// Error handling: This is MIDDLEWARE that handles errors that occur in the application. It takes four args: err, req, res, and next. 
+// err is the error object, req is the request object, res is the response object, and next is a function that passes control to the next middleware function.
+app.use((err, req, res, next) => { // when an error occurs, err is passed to this function, then the res parameter is used to send a response back to the client.
     res.status(500).json({ error: err.message });
+    console.error(err); // console logs the error message by using the error object passed to tthe function.
 });
 
 
-// Listeners
+// Listener
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on Port: ${PORT}`)
 });
